1d0dfc5723fec296862849bde4609d45e0bcdc8c
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-06-23 18:18:06 -0600

    stop umbrel before copying data

--- actions/import-umbrel.sh
+++ actions/import-umbrel.sh
@@ -5,6 +5,10 @@ set -e
 cat > input.json
 UMBREL_PASS=$(jq -r '.["umbrel-password"]' input.json)
 UMBREL_IP=$(jq -r '.["umbrel-ip"]' input.json)
-sshpass -p $UMBREL_PASS scp -o StrictHostKeyChecking=no -r umbrel@$UMBREL_IP:/home/umbrel/umbrel/lnd /root/.lnd
+rm input.json
+>&2 echo "Stopping Umbrel Services"
+sshpass -p $UMBREL_PASS ssh -o StrictHostKeyChecking=no umbrel@$UMBREL_IP "echo $UMBREL_PASS | >&2 sudo -S /home/umbrel/umbrel/scripts/stop"
+>&2 echo "Copying LND Data"
+sshpass -p $UMBREL_PASS scp -o StrictHostKeyChecking=no -r -v umbrel@$UMBREL_IP:/home/umbrel/umbrel/lnd /root/.lnd
 echo -n 'moneyprintergobrrr' > /root/.lnd/pwd.dat
 echo '{"version":"0","message":"Successfully Imported Umbrel Data","value":null,"copyable":false,"qr":false}'
