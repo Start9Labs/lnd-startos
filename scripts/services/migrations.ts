e219579ecf55d414ef33cf95179f5eaecde1c057
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-07-15 18:46:22 -0600

    handle all migrations in ts file, organized for clarity around up/down migrations

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -34,45 +34,52 @@ export const migration: T.ExpectedExports.migration = async (effects, version) =
 
     // TODO Fix version checking when proper Emver range parsing/comparisons implemented
 
+
+    // from migrations (upgrades)
     if (version <= '0.14.2.1') {
         // ie. if neutrino
         if (parsed.bitcoind.type === 'none') {
             parsed.bitcoind.type = 'internal-proxy'
         }
-    }
 
-    // if bitcoin is configured to internal (ie. pointer to proxy), upgrade should ensure it remains proxy. As of 0.14.2, internal means bitcoin core. 
-    if (version <= '0.13.3.2') {
-        if (parsed.bitcoind.type === 'internal') {
+        // if bitcoin is configured to internal (ie. pointer to proxy), upgrade should ensure it remains proxy. As of 0.14.2, internal means bitcoin core. 
+        if (version <= '0.13.3.2') {
+            if (parsed.bitcoind.type === 'internal') {
+                parsed.bitcoind.type = 'internal-proxy'
+            }
+        }
+
+        // handle cases prior to 0.14.2.1 when external was still an option 
+        if (parsed.bitcoind.type === 'external') {
             parsed.bitcoind.type = 'internal-proxy'
+        }
+
+        await effects.writeFile({
+            volumeId: "main",
+            path: "start9/config.yaml",
+            toWrite: YAML.stringify(parsed)
+        })
+
+        // tor was added in 0.14.2
+        if (version < '0.14.2') {
+            if (!matchTor.test(parsed)) {
+                return { result: { configured: false } }
+            }
+            // watchtower options were removed in 0.14.2
+            if (!matchWatch.test(parsed)) {
+                return { result: { configured: false } }
+            }
         }
     }
 
-    // handle cases prior to 0.14.2.1 when external was still an option 
-    if (parsed.bitcoind.type === 'external') {
-        parsed.bitcoind.type = 'internal-proxy'
-    }
-
-    await effects.writeFile({
-        volumeId: "main",
-        path: "start9/config.yaml",
-        toWrite: YAML.stringify(parsed)
-    })
-
-
-    // tor was added in 0.14.2 - first case is for upgrades, second for downgrades
-    if (version < '0.14.2' || version >= '0.14.2.1') {
+    // to migrations (downgrades)
+    if (version > '0.14.2.1') {
+        // tor was added in 0.14.2 
         if (!matchTor.test(parsed)) {
             return { result: { configured: false } }
         }
     }
 
-    // watchtower options were removed in 0.14.2
-    if (version < '0.14.2') {
-        if (!matchWatch.test(parsed)) {
-            return { result: { configured: false } }
-        }
-    }
-
     return { result: { configured: true } }
+
 }
