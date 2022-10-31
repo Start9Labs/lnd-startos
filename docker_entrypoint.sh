5936b85471847671ee61f4c6e561b4225501c5d6
Dread <dread@start9labs.com>
2022-10-31 16:18:21 -0400

    hotfix for peers on first run

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,10 +12,25 @@ _term() {
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 export CONTAINER_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
+# ----------Hotfix for bootstrapping peers----------
+export LND_PEERS='lncli connect 03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f@3.33.236.230:9735 && lncli connect 02bba49d7f9c57b9c05f7eb33bf4dc69b2aa37cb63caff93f13bfa88135e7f7a46@212.129.58.219:9739 && lncli connect 033dee9c6a0afc40ffd8f27d68ef260f3e5e1c19e59c6f9bb607fb04c1d497a809@98.165.150.209:9735'
+# ----------End of Hotfix---------------------------
+
 configurator
 configurator_child=$!
 lnd &
 lnd_child=$!
+
+# ----------Hotfix for bootstrapping peers----------
+until true; do {
+SYNC_CHECK=$(lncli getinfo)
+case $SYNC_CHECK in
+"$CHECK_VAR") break ;;
+*) echo waiting to add peers... ;;
+esac
+} done
+$LND_PEERS &
+# ----------End of Hotfix---------------------------
 
 trap _term SIGTERM
 
