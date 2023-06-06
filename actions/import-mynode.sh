c69ef52948f02c802031227bd531e19ee1b9a657
Shadowy Super Coder <musashidisciple@proton.me>
2023-06-06 15:10:21 -0600

    fix wildcard placement

--- actions/import-mynode.sh
+++ actions/import-mynode.sh
@@ -9,7 +9,7 @@ rm input.json
 >&2 echo "Stopping MyNode Services"
 sshpass -p $MYNODE_PASS ssh -o StrictHostKeyChecking=no admin@$MYNODE_HOST "echo $MYNODE_PASS | >&2 sudo -S /usr/bin/mynode_stop_critical_services.sh"
 >&2 echo "Copying LND data"
-sshpass -p $MYNODE_PASS scp -o StrictHostKeyChecking=no -r -v admin@$MYNODE_HOST:/mnt/hdd/mynode/lnd/data* /root/.lnd
+sshpass -p $MYNODE_PASS scp -o StrictHostKeyChecking=no -r -v admin@$MYNODE_HOST:/mnt/hdd/mynode/lnd/data/* /root/.lnd
 LN_CLI_PASS=$(sshpass -p "$MYNODE_PASS" ssh -o StrictHostKeyChecking=no admin@$MYNODE_HOST "cat /mnt/hdd/mynode/settings/.lndpw")
 echo -n "$LN_CLI_PASS" > /root/.lnd/pwd.dat
 echo '{"version":"0","message":"Successfully Imported MyNode Data","value":null,"copyable":false,"qr":false}'
