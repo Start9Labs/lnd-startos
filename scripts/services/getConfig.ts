8fb9a3a40f1788febc72f250610a0a7820215805
Shadowy Super Coder <musashidisciple@proton.me>
2023-10-30 17:17:39 -0600

    Refactor watchtowers config and configurator

--- scripts/services/getConfig.ts
+++ scripts/services/getConfig.ts
@@ -263,24 +263,41 @@ export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
         "default": false,
       },
       "wt-client": {
-        "type": "boolean",
+        "type": "union",
         "name": "Enable Watchtower Client",
         "description":
           "Allow your node to find other watchtower servers on the network.",
-        "default": false,
-      },
-      "add-watchtowers": {
-        "type": "list",
-        "name": "Add Watchtowers",
-        "description": "Add URIs of Watchtowers to connect to.",
-        "range": "[0,*)",
-        "subtype": "string",
-        "spec": {
-          "masked": false,
-          "copyable": true,
-          "placeholder": "pubkey@host",
+        // "nullable": true,
+        tag: {
+          id: "enabled",
+          name: "Watchtower Client Enabled",
+          description: "- Disabled: Disable Watchtower Client\n-Enable Watchtower Client",
+          "variant-names": {
+            disabled: "Disabled",
+            enabled: "Enabled",
+          },
         },
-        "default": [],
+        "default": "disabled",
+        variants: {
+          disabled: {},
+          enabled: {
+            "add-watchtowers": {
+              "type": "list",
+              "name": "Add Watchtowers",
+              "description":
+                "Add URIs of Watchtowers to connect to.",
+              "range": "[1,*)",
+              "subtype": "string",
+              "spec": {
+                "masked": false,
+                "copyable": true,
+                "placeholder":
+                  "pubkey@host:9911",
+              },
+              "default": Array<string>(),
+            },
+          }
+        }
       },
     },
   },
