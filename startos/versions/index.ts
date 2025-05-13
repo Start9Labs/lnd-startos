c64257a4d44716b60d8f38f7f674b5a4ef32a777
Shadowy Super Coder <musashidisciple@proton.me>
2025-05-13 11:58:01 -0600

    bump StartOS pkg exver

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,4 +1,4 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v0_18_5_beta } from './v0.18.5-beta'
+import { v0_18_5_beta_1 } from './v0.18.5-beta_1'
 
-export const versions = VersionGraph.of(v0_18_5_beta)
+export const versions = VersionGraph.of(v0_18_5_beta_1)
