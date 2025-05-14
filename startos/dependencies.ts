ecda89dea3457ed2d304d704ae75fa10bd071242
Shadowy Super Coder <musashidisciple@proton.me>
2025-05-14 00:49:10 -0600

    caret bitcoind dep

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -17,7 +17,7 @@ export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
       bitcoind: {
         healthChecks: ['sync-progress'],
         kind: 'running',
-        versionRange: '>=28.1.0-0',
+        versionRange: '^28.1.0-0',
       },
     }
   }
