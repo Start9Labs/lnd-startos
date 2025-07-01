13ec8eef8baede9d91bf81627ed163a4c7584496
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-01 15:36:50 -0600

    update link for bitcoind s9pk

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -38,7 +38,7 @@ export const manifest = setupManifest({
     bitcoind: {
       description: 'Used to subscribe to new block events.',
       optional: true,
-      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.4/bitcoind.s9pk',
+      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.5/bitcoind.s9pk',
     },
   },
 })
