ea95c7e24f1b529dbd00e81f21c177f9f7a794ff
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-09 14:34:37 -0600

    add bitcoind primary health check

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -20,7 +20,7 @@ export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
 
     return {
       bitcoind: {
-        healthChecks: ['sync-progress'],
+        healthChecks: ['sync-progress', 'primary'],
         kind: 'running',
         versionRange: '>=28.1:3-alpha.6',
       },
