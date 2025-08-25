7323381f15a9fe5bfd92f04cd20c635f1d51cccb
Shadowy Super Coder <musashidisciple@proton.me>
2025-08-25 14:56:31 -0600

    beta.2

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
-export { v0_19_2_beta_1_beta_1 as current } from './v0.19.2-beta_1-beta.1'
+export { v0_19_2_beta_1_beta_2 as current } from './v0.19.2-beta_1-beta.2'
 import { v0_19_2_beta_0 } from './v0.19.2-beta_1-beta.0'
 
 export const other = [v0_19_2_beta_0]
--- startos/install/versions/v0.19.2-beta_1-beta.2.ts
+++ startos/install/versions/v0.19.2-beta_1-beta.2.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v0_19_2_beta_1_beta_1 = VersionInfo.of({
-  version: '0.19.2-beta:1-beta.1',
+export const v0_19_2_beta_1_beta_2 = VersionInfo.of({
+  version: '0.19.2-beta:1-beta.2',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
